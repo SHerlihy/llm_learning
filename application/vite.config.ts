c86788b64ecb79701b20b487770eb70d31602e31
SHerlihy <steven_herlihy@yahoo.com>
2026-03-07 09:14:30 +0000

    build as SPA

--- application/vite.config.ts
+++ application/vite.config.ts
@@ -8,19 +8,23 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    // react's vite plugin must come after start's vite plugin
-    viteReact(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        port: 3000,
     },
-  },
+    plugins: [
+        tsConfigPaths(),
+        tanstackStart({
+            spa: {
+                enabled: true,
+            },
+        }),
+        // react's vite plugin must come after start's vite plugin
+        viteReact(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
