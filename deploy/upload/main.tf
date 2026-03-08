adbdd15e574d9fb5da45aaef7f7f7327f4459bac
SHerlihy <steven_herlihy@yahoo.com>
2026-03-08 19:36:54 +0000

    rely on default creds

--- deploy/upload/main.tf
+++ deploy/upload/main.tf
@@ -11,18 +11,8 @@ variable "region" {
   type = string
 }
 
-variable "access_key" {
-  type = string
-}
-
-variable "secret_key" {
-  type = string
-}
-
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 variable "bucket_id" {
