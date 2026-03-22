c8046585220144d2bd67440ce017e236cd9446b6
SHerlihy <steven_herlihy@yahoo.com>
2026-03-22 16:40:05 +0000

    added resources

---
+++ application/src/content/resources/w3_march_26/aiVideoGeneratorComparison.ts
@@ -0,0 +1,11 @@
+import { ResourceBuilder } from "@/content/resourceBuilder"
+
+const aiVideoGeneratorComparison = new ResourceBuilder('Kling 3.0 vs Seedance 2 vs Veo 3.1 vs Sora 2: The Ultimate AI Video Comparison by Dom the AI TUtor | Tech Tutor Zones')
+.setHref('https://youtu.be/-MluR9dqt5w?si=fYxQB48vYvmrKELo')
+.setDateOfInclusion(new Date(2026, 3, 22, 16))
+.setOverview('Video comparing vidoes created by major AI video generators across multiple prompts.')
+.setTag('chill')
+.setTag('art')
+.build()
+
+export default aiVideoGeneratorComparison
---
+++ application/src/content/resources/w3_march_26/index.ts
@@ -0,0 +1,14 @@
+import { Resource } from "@/content/resourceBuilder";
+import aiFilmFestivalCritique from "./aiFilmFestivalCritique";
+import promptingGeneralAdvice from "./promptingGeneralAdvice";
+import cybersecurityByAICritique from "./cybersecurityByAICritique";
+import aiImagesAndVideosHowItsMade from "./aiImagesAndVideosHowItsMade";
+import aiVideoGeneratorComparison from "./aiVideoGeneratorComparison";
+
+export const w3March26Resources: Array<Resource> = [
+    aiFilmFestivalCritique,
+    promptingGeneralAdvice,
+    cybersecurityByAICritique,
+    aiImagesAndVideosHowItsMade,
+    aiVideoGeneratorComparison
+]
