603144fb07ad61186ddce38cf77d75b20c8052c0
SHerlihy <steven_herlihy@yahoo.com>
2026-03-30 08:25:43 +0100

    add overview

--- application/src/content/resources/w4_march_26/awsPromptCaching.ts
+++ application/src/content/resources/w4_march_26/awsPromptCaching.ts
@@ -3,6 +3,7 @@ import { ResourceBuilder } from "@/content/resourceBuilder";
 const awsPromptCaching = new ResourceBuilder('Prompt caching for faster model inference by AWS')
 .setHref('https://docs.aws.amazon.com/bedrock/latest/userguide/prompt-caching.html')
 .setDateOfInclusion(new Date(2026, 3, 30, 8))
+.setOverview('How it works for using AWS services to cache parts of a prompt.')
 .setTag('cost')
 .setTag('coding')
 .setTag('prompting')
