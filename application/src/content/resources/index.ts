b0bcd254534e95f2a34486a11fb27a675521720f
SHerlihy <steven_herlihy@yahoo.com>
2026-03-30 08:18:19 +0100

    add resources

--- application/src/content/resources/index.ts
+++ application/src/content/resources/index.ts
@@ -2,9 +2,11 @@ import { Resource } from "@/content/resourceBuilder";
 import { w1March26Resources } from "./w1_march_26";
 import { w2March26Resources } from "./w2_march_26";
 import { w3March26Resources } from "./w3_march_26";
+import { w4March26Resources } from "./w4_march_26";
 
 export const allResources: Array<Resource> = [
     ...w1March26Resources,
     ...w2March26Resources,
-    ...w3March26Resources
+    ...w3March26Resources,
+    ...w4March26Resources
 ]
---
+++ application/src/content/resources/w4_march_26/index.ts
@@ -0,0 +1,11 @@
+import claudeRateLimits from "./claudeRateLimits";
+import costRagPromptFine from "./costRagPromptFine";
+import googleCompressionAlgo from "./googleCompressionAlgo";
+import soraFinancials from "./soraFinancials";
+
+export const w4March26Resources = [
+    costRagPromptFine,
+    soraFinancials,
+    googleCompressionAlgo,
+    claudeRateLimits
+]
