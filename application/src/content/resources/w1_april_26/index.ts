953d2aaf7327509c8e06300a87fe93c852174138
SHerlihy <steven_herlihy@yahoo.com>
2026-04-03 12:50:31 +0100

    add web traffic resources

---
+++ application/src/content/resources/w1_april_26/index.ts
@@ -0,0 +1,8 @@
+import { Resource } from "@/content/resourceBuilder";
+import webtrafficVaiOverviews from "./webtrafficVaiOverviews";
+import tailwindVaiOverview from "./tailwindVaiOverviews";
+
+export const w1April26Resources: Array<Resource> = [
+    webtrafficVaiOverviews,
+    tailwindVaiOverview
+]
