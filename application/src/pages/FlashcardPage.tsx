851ed52eb9846371cc94aca527bdac534ca293d6
SHerlihy <steven_herlihy@yahoo.com>
2026-04-05 22:09:04 +0100

    get something working

---
+++ application/src/pages/FlashcardPage.tsx
@@ -0,0 +1,85 @@
+import { Button } from '@/components/ui/button'
+import { nameToResource } from '@/content'
+import { useSearch } from '@tanstack/react-router'
+import { useState } from 'react'
+import FlashcardGrader from './FlashcardGrader'
+
+const FlashcardPage = () => {
+    const { resourceName } = useSearch({ from: '/flashcard' })
+    const resource = nameToResource[resourceName]
+
+    const flashcards = resource.flashcards
+    const flashcardsAmount = Object.entries(flashcards).length
+
+    if (flashcardsAmount < 1) {
+        return <p>Sorry, no flashcards for {resourceName}</p>
+    }
+
+    const flashcardsArr = Object.entries(flashcards)
+        .sort(() => Math.random() - 0.5);
+
+    return (
+        <FlashcardSelection flashcardsArr={flashcardsArr} />
+    )
+}
+
+type Props = {
+    flashcardsArr: string[][],
+}
+
+const FlashcardSelection = ({
+    flashcardsArr
+}: Props) => {
+    const [flashcardIdx, setFlashcardIdx] = useState(0)
+
+    const goBack = () => {
+        setFlashcardIdx((prev) => {
+            if (prev < 1) {
+                return flashcardsArr.length - 1
+            }
+
+            return prev - 1
+        })
+    }
+
+    const goForward = () => {
+        setFlashcardIdx((prev) => {
+            const nextIdx = prev + 1
+            if (nextIdx > flashcardsArr.length - 1) {
+                return 0
+            }
+
+            return nextIdx
+        })
+    }
+
+    return (
+        <article>
+            <h2>
+                {flashcardsArr[flashcardIdx][0]}
+            </h2>
+            <section>
+                <FlashcardGrader
+                    keyword={flashcardsArr[flashcardIdx][0]}
+                    definition={flashcardsArr[flashcardIdx][1]}
+                />
+            </section>
+            <section
+                className='flex'
+            >
+                <Button
+                    onClick={goBack}
+                >
+                    Back
+                </Button>
+                <Button
+                    onClick={goForward}
+                >
+                    Next
+                </Button>
+            </section>
+        </article>
+    )
+}
+
+export default FlashcardPage
--- application/src/routes/flashcard.tsx
+++ application/src/routes/flashcard.tsx
@@ -1,6 +1,12 @@
-import Flashcard from '@/pages/Flashcard'
+import FlashcardPage from '@/pages/FlashcardPage'
 import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
+
+const schema = z.object({
+    resourceName: z.string()
+})
 
 export const Route = createFileRoute('/flashcard')({
-    component: Flashcard
+    component: FlashcardPage,
+    validateSearch: schema
 })
