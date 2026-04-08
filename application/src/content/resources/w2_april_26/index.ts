e0b19f3021f6fa73cff7a955d8b9615f7d6a8997
SHerlihy <steven_herlihy@yahoo.com>
2026-04-08 08:12:18 +0100

    add ai phrases article

---
+++ application/src/content/resources/w2_april_26/index.ts
@@ -0,0 +1,6 @@
+import { Resource } from "@/content/resourceBuilder";
+import aiPhrasesFromWikipedia from "./aiPhrasesFromWikipedia";
+
+export const w2April26Resources: Array<Resource> = [
+    aiPhrasesFromWikipedia
+]
