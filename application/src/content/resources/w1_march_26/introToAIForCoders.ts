3abc48ef57a70e98cde4b71682555c6422ea6b40
SHerlihy <steven_herlihy@yahoo.com>
2026-04-09 06:50:13 +0100

    remove test flashcards

--- application/src/content/resources/w1_march_26/introToAIForCoders.ts
+++ application/src/content/resources/w1_march_26/introToAIForCoders.ts
@@ -4,9 +4,6 @@ const introToAIForCoders = new ResourceBuilder("Enterprise AI Tutorial – Embed
 .setHref("https://youtu.be/HaUe2AN210g?si=SXu6H8Bv5EhhcL8Z")
 .setOverview("A light introduction to the software required for LLMs and mutli-modal LLMs to function.")
 .setDateOfInclusion(new Date(2026, 3, 9))
-.setFlashcard("inference", "a conclusion reached on the basis of evidence and reasoning.")
-.setFlashcard("BERT", "a bidirectional model designed to understand the deep context of words by reading entire sentences in both directions simultaneously.")
-.setFlashcard("GPT", "a unidirectional model designed to generate coherent, human-like text by predicting the next word in a sequence based on the context of preceding words.")
 .setTag("coding")
 .build()
 
