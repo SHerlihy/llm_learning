b9b8e12bb5c08c1882013f95da61e1bb61e989eb
SHerlihy <steven_herlihy@yahoo.com>
2026-04-12 14:41:15 +0100

    add resources

---
+++ application/src/content/resources/w2_april_26/chatGptAndMassMurder.ts
@@ -0,0 +1,10 @@
+import { ResourceBuilder } from "@/content/resourceBuilder";
+
+const chatGptAndMassMurder = new ResourceBuilder('ChatGPT helped kіller nazі іncel plan school shooting by Caelan Conrad')
+.setDateOfInclusion(new Date(2026, 4, 12, 14))
+.setOverview('Video exploring the sad details of a mass shooting in a school that was pontentially enabled by ChatGPT.')
+.setHref('https://youtu.be/IbG-JGdGniY?si=SKYkhWZxAni4bt18')
+.setTag('ethics')
+.build()
+
+export default chatGptAndMassMurder
--- application/src/content/resources/w2_april_26/index.ts
+++ application/src/content/resources/w2_april_26/index.ts
@@ -1,6 +1,10 @@
 import { Resource } from "@/content/resourceBuilder";
 import aiPhrasesFromWikipedia from "./aiPhrasesFromWikipedia";
+import aiWritingAsistantComparison from "./aiWritingAssistantComparison";
+import chatGptAndMassMurder from "./chatGptAndMassMurder";
 
 export const w2April26Resources: Array<Resource> = [
-    aiPhrasesFromWikipedia
+    aiPhrasesFromWikipedia,
+    aiWritingAsistantComparison,
+    chatGptAndMassMurder
 ]
