f3879f0d74c7cb4c0e4cf6631ebd39523a0b1ad9
SHerlihy <steven_herlihy@yahoo.com>
2026-04-13 07:49:02 +0100

    add ai hack games

---
+++ application/src/content/resources/w2_april_26/gameForHackingAis.ts
@@ -0,0 +1,12 @@
+import { ResourceBuilder } from "@/content/resourceBuilder";
+
+const gameForHackingAis = new ResourceBuilder('Play Games to Hack and Manipulate AIs by Lakera')
+.setHref('https://gandalf.lakera.ai/intro')
+.setOverview('I\'ve had so much fun getting a wellness app AI to swear loads.')
+.setDateOfInclusion(new Date(2026, 4, 12, 18))
+.setTag('chill')
+.setTag('security')
+.setTag('prompting')
+.build()
+
+export default gameForHackingAis
--- application/src/content/resources/w2_april_26/index.ts
+++ application/src/content/resources/w2_april_26/index.ts
@@ -2,9 +2,11 @@ import { Resource } from "@/content/resourceBuilder";
 import aiPhrasesFromWikipedia from "./aiPhrasesFromWikipedia";
 import aiWritingAsistantComparison from "./aiWritingAssistantComparison";
 import chatGptAndMassMurder from "./chatGptAndMassMurder";
+import gameForHackingAis from "./gameForHackingAis";
 
 export const w2April26Resources: Array<Resource> = [
     aiPhrasesFromWikipedia,
     aiWritingAsistantComparison,
-    chatGptAndMassMurder
+    chatGptAndMassMurder,
+    gameForHackingAis
 ]
