147e8a6bfc898ffd9fb34786c7d9a73c98dd8cf1
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-21 22:44:37 +0100

    show correct content

--- application/src/features/listResources/ResourceList.tsx
+++ application/src/features/listResources/ResourceList.tsx
@@ -1,5 +1,5 @@
 import { useSearch } from "@tanstack/react-router"
-import { allResourceNames, Tag, tagToNamesByDoI } from "@/content"
+import { tags as allTags, allResourceNames, Tag, tagToNamesByDoI } from "@/content"
 import ResourceListView from "./ResourceListView"
 
 const getResourceIntersectionFromTags = (tags: Array<Tag>) => {
@@ -23,19 +23,23 @@ const getResourceIntersectionFromTags = (tags: Array<Tag>) => {
 
 //composite component to show feedback components
 const ResourceList = () => {
-    const { tags: tagsObj } = useSearch({ from: '/' })
+    const { selectedTags } = useSearch({ from: '/' })
 
-    const tags: Array<Tag> = []
+  let sTags = selectedTags
 
-    if (tagsObj !== undefined){
-      Object.entries(tagsObj).forEach(([tag, val])=>{
-      if(val===false){
-        return
-      }
+  if (sTags===undefined){
+    sTags=0
+  }
 
-      // @ts-ignore
-      tags.push(tag)
-    })
+  const tags: Array<Tag> = []
+  let idx = 0
+  while (sTags > 0){
+    sTags--
+    while (sTags % 2 > 0){
+      sTags = sTags >> 1
+      idx++
+    }
+    tags.push(allTags[idx])
   }
 
     const intersectionTaged = getResourceIntersectionFromTags(tags)
